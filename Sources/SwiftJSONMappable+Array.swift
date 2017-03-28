41c5fde043a85ac5033accac9f3fa7ab95491b76
Arror <hallo.maqiang@gmail.com>
2017-03-28 22:36:14 +0800

    Remove prefix

--- Sources/SwiftJSONMappable+Array.swift
+++ Sources/SwiftJSONMappable+Array.swift
@@ -1,11 +1,3 @@
-//
-//  SwiftJSONMappable+Array.swift
-//  Sample
-//
-//  Created by 马强 on 2016/12/31.
-//  Copyright © 2016年 马强. All rights reserved.
-//
-
 import Foundation
 
 extension Array where Element: SwiftJSONMappable {
--- Sources/SwiftJSONMappable.swift
+++ Sources/SwiftJSONMappable.swift
@@ -1,11 +1,3 @@
-//
-//  SwiftJSONMappable.swift
-//  Sample
-//
-//  Created by 马强 on 2016/12/31.
-//  Copyright © 2016年 马强. All rights reserved.
-//
-
 import Foundation
 
 public protocol SwiftJSONMappable {
