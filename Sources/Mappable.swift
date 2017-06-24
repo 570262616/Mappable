40fa52f72c1a1d83f28e369de8c850bbfacf9865
Arror <hallo.maqiang@gmail.com>
2017-06-24 14:45:24 +0800

    Add SwiftJSON

--- Sources/Mappable.swift
+++ Sources/Mappable.swift
@@ -27,27 +27,3 @@ extension EnumMappable {
     
     public var json: Any { return self.rawValue }
 }
-
-public struct SwiftJSON {
-    
-    private let storage: [String: Any]
-    
-    public init?(_ any: Any?) {
-        
-        guard let dict = any as? [String: Any] else { return nil }
-        
-        self.storage = dict
-    }
-    
-    public subscript<T: Mappable>(_ key: String) -> T? {
-        
-        return T(any: self.storage[key])
-    }
-    
-    public subscript<T: Collection>(_ key: String) -> T? where T.Element: Mappable {
-        
-        guard let elements = self.storage[key] as? [Any] else { return nil }
-        
-        return elements.flatMap { T.Element(any: $0) } as? T
-    }
-}
